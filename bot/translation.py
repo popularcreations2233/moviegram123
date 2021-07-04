20ebb5b1b7295b47b9c74052829c396493ddcc30
popularcreations2233 <86342803+popularcreations2233@users.noreply.github.com>
2021-07-04 08:03:54 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -30,11 +30,11 @@ Just Send Any Text Will Try To Lookup In Channel And Provide You The Link</i>
     
     ABOUT_TEXT = """<b>➥ Name</b> : <code>Adv Auto Filter Bot</code>
 
-<b>➥ Creator</b> : <b><i><a herf="https://t.me/AlbertEinstein_TG">AlbertEinstein_TG</a></i></b>
+<b>➥ Creator</b> : <b><i><a herf=https://github.com/popularcreations2233/moviegram123</a></i></b>
 
 <b>➥ Language</b> : <code>Python3</code>
 
 <b>➥ Library</b> : <i><a herf="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0</a></i>
 
-<b>➥ Source Code</b> : <i><a herf="https://github.com/AlbertEinsteinTG/Adv-Auto-Filter-Bot">GitHub</a></i>
+<b>➥ Source Code</b> : <i><a herf="https://github.com/popularcreations2233/moviegram123">GitHub</a></i>
 """
