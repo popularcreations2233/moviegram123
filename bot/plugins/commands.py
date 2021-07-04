904940043c0f0619a076e8c4988524049e9b299b
popularcreations2233 <86342803+popularcreations2233@users.noreply.github.com>
2021-07-04 08:24:11 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -4,13 +4,92 @@
 
 from pyrogram import filters, Client
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery
-from bot.translation import Translation # pylint: disable=import-error
+from bot import Translation # pylint: disable=import-error
+from bot.database import Database # pylint: disable=import-error
 
-@Client.on_message(filters.command("start") & filters.private)
+db = Database()
+
+@Client.on_message(filters.command(["start"]) & filters.private, group=1)
 async def start(bot, update):
     
+    try:
+        file_uid = update.command[1]
+    except IndexError:
+        file_uid = False
+    
+    if file_uid:
+        file_id, file_name, file_caption, file_type = await db.get_file(file_uid)
+        
+        if (file_id or file_type) == None:
+            return
+        
+        caption = file_caption if file_caption != ("" or None) else ("<code>" + file_name + "</code>")
+        
+        if file_type == "document":
+        
+            await bot.send_document(
+                chat_id=update.chat.id,
+                document = file_id,
+                caption = caption,
+                parse_mode="html",
+                reply_to_message_id=update.message_id,
+                reply_markup=InlineKeyboardMarkup(
+                    [
+                        [
+                            InlineKeyboardButton
+                                (
+                                    'Developers', url="https://t.me/movie_gram123"
+                                )
+                        ]
+                    ]
+                )
+            )
+
+        elif file_type == "video":
+        
+            await bot.send_video(
+                chat_id=update.chat.id,
+                video = file_id,
+                caption = caption,
+                parse_mode="html",
+                reply_markup=InlineKeyboardMarkup(
+                    [
+                        [
+                            InlineKeyboardButton
+                                (
+                                    'Developers', url="https://t.me/movie_gram123"
+                                )
+                        ]
+                    ]
+                )
+            )
+            
+        elif file_type == "audio":
+        
+            await bot.send_audio(
+                chat_id=update.chat.id,
+                audio = file_id,
+                caption = caption,
+                parse_mode="html",
+                reply_markup=InlineKeyboardMarkup(
+                    [
+                        [
+                            InlineKeyboardButton
+                                (
+                                    'Developers', url="https://t.me/movie_gram123"
+                                )
+                        ]
+                    ]
+                )
+            )
+
+        else:
+            print(file_type)
+        
+        return
+
     buttons = [[
-        InlineKeyboardButton('My Dev 👨‍🔬', url='https://t.me/movie_gram123'),
+        InlineKeyboardButton('Developers', url='https://t.me/movie_gram123'),
         InlineKeyboardButton('Source Code 🧾', url ='https://github.com/popularcreations2233/moviegram123')
     ],[
         InlineKeyboardButton('Support 🛠', url='https://t.me/movie_gram123')
@@ -29,7 +108,8 @@ async def start(bot, update):
         reply_to_message_id=update.message_id
     )
 
-@Client.on_message(filters.command("help") & filters.private)
+
+@Client.on_message(filters.command(["help"]) & filters.private, group=1)
 async def help(bot, update):
     buttons = [[
         InlineKeyboardButton('Home ⚡', callback_data='start'),
@@ -48,18 +128,21 @@ async def help(bot, update):
         reply_to_message_id=update.message_id
     )
 
-@Client.on_message(filters.command("about") & filters.private)
+
+@Client.on_message(filters.command(["about"]) & filters.private, group=1)
 async def about(bot, update):
+    
     buttons = [[
         InlineKeyboardButton('Home ⚡', callback_data='start'),
         InlineKeyboardButton('Close 🔐', callback_data='close')
     ]]
     reply_markup = InlineKeyboardMarkup(buttons)
+    
     await bot.send_message(
         chat_id=update.chat.id,
         text=Translation.ABOUT_TEXT,
         reply_markup=reply_markup,
         disable_web_page_preview=True,
-        parse_mode="html", 
+        parse_mode="html",
         reply_to_message_id=update.message_id
     )
